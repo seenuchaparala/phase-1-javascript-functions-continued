8aae2dedd34c6a459d53a3ebbe1d8f3caf49f1e5
Srinivas <seenuchaparala@gmail.com>
2022-03-15 16:06:38 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(fun = `roller-skate`) {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+const mondayWork = function (work = `go to the office`){
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(char = `*`) {
+    const innerFunction = function(wish = `special`){
+        return `You are ${char}${wish}${char}!`;
+    }
+    return innerFunction;
+}
+
+console.log(wrapAdjective(`$`)(`test`));
